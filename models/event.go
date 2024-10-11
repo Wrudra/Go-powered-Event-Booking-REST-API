d586a3d16738b8c8937526e6e284bdca5ceba92e
Tahsinul Haque Wrudra <rudra483haque@gmail.com>
2024-10-12 01:03:17 +0600

    Create event.go

---
+++ models/event.go
@@ -0,0 +1,15 @@
+package models //All the logic that deals with storing event data in a database
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string
+	Description string
+	Location    string
+	DateTime    time.Time
+	UserID      int
+}
+
+func Save() {
+	
+}
