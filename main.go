e44d63c05e2bc18409e324862143c20222fde8ad
Tahsinul Haque Wrudra <rudra483haque@gmail.com>
2024-10-12 17:27:53 +0600

    Testing requests and fixing the POST request handler

--- main.go
+++ main.go
@@ -78,5 +78,7 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event}) //"event":event sending back that event that was created
 }
